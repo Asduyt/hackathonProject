2d0a85f954d233b73bdb61aaaa133631d8eba3d4
Asduyt <133279299+Asduyt@users.noreply.github.com>
2023-09-24 00:58:42 -0400

    ADded Vigenere Encryption

--- veryeasycrypt/src/App.js
+++ veryeasycrypt/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import DropdownMenu from './Dropdown';
 
 let input;
 let output;
@@ -8,23 +7,14 @@ function App() {
   const [word, setWord] = useState("");
   const [password, setPassword] = useState("");
   const [isEncrypted, setIsEncrypted] = useState(true);
-  const [encryptionType, setType] = useState("Caesar Cipher");
 
   const handleSubmit = (e) => {
     e.preventDefault()
     input = word;
     if (isEncrypted) {
-      if (encryptionType === "Caesar Cipher"){  
-        setPassword(encryptCaesar(input));
-      } else {
-        setPassword(encrypt2(input));
-      }
+      setPassword(encryptCaesar(input));
     } else {
-      if (encryptionType === "Caesar Cipher") {
-        setPassword(decryptCaesar(input));
-      } else {
-        setPassword(decrypt2(input));
-      }
+      setPassword(decryptCaesar(input));
     }
   };
 
@@ -39,14 +29,10 @@ function App() {
     setPassword("");
   };
 
-  const changeType = (e) => {
-    setType(e.target.value);
-  }
-
   return (
     <div>
       <div className="crypt-title">
-        <h2>VeryEasyCrypt</h2>
+        <h3>VeryEasyCrypt</h3>
       </div>
       <div className="button-container">
         <button
@@ -64,10 +50,6 @@ function App() {
           DECRYPT
         </button>
       </div>
-
-      <DropdownMenu changeType={changeType}/>
-
-
       <form className="grocery-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <input
@@ -96,28 +78,18 @@ function App() {
           </button>
         </div>
       </form>
-
-      <button className="clear-btn">
-        Save
-      </button>
-
-
     </div>
   );
 }
 
-function encrypt2(word) {
-  return "blank";
-}
-
-function decrypt2(word) {
-  return "blank";
-}
-
+//Encrypt a given word using a Caesar Cypher with a key of 2
 function encryptCaesar(word) {
   var encryptedWord = "";
 
+  //Iterate through each letter of the word
   for (let i = 0; i < word.length; i++) {
+
+    //Account for spaces
     if (word[i] === " "){
       encryptedWord += " ";
       i++;
@@ -125,25 +97,39 @@ function encryptCaesar(word) {
     var letter = word.charAt(i);
     var asciiNum = letter.charCodeAt(0);
     var newNum = asciiNum + 2;
+
+    //Check if the char is lowercase
     if (asciiNum < 91 && asciiNum > 64) {
+
+      //Check if the char is out of bounds and fix it
       if (newNum > 90) {
         newNum -= 26;
       }
     }
+
+    //Check if the char is uppercase
     if (asciiNum < 123 && asciiNum > 96) {
+
+      //Check if the char is out of bounds and fix it
       if (newNum > 122) {
         newNum -= 26;
       }
     }
+
+    //Append the encrypted letter to a string
     encryptedWord += String.fromCharCode(newNum);
   }
   return encryptedWord;
 }
 
+//Decrypt an encrypted Caesar Cypher string
 function decryptCaesar(word) {
   var decryptedWord = "";
 
+  ////Iterate through each letter of the string
   for (let i = 0; i < word.length; i++) {
+    
+    //Account for spaces
     if (word[i] === " "){
       decryptedWord += " ";
       i++;
@@ -151,19 +137,66 @@ function decryptCaesar(word) {
     var letter = word.charAt(i);
     var asciiNum = letter.charCodeAt(0);
     var newNum = asciiNum - 2;
+
+    //Check if the char is lowercase
     if (asciiNum < 91 && asciiNum > 64) {
+
+      //Check if the char is out of bounds and fix it
       if (newNum < 65) {
         newNum += 26;
       }
     }
+    //Check if the char is uppercase
     else if (asciiNum < 123 && asciiNum > 96) {
+
+      //Check if the char is out of bounds and fix it
       if (newNum < 97) {
         newNum += 26;
       }
     }
+
+    //Append decrypted letter to a string
     decryptedWord += String.fromCharCode(newNum);
   }
   return decryptedWord;
 }
 
+function encryptVigenere(word, key){
+  var rows = 26;
+  var cols = 26;
+  var vigenereArray = [];
+  var keyCount = 0;
+  var tempWord = word.toUpperCase();
+  encryptedWord = "";
+
+  for(int i = 0; i < rows; i++){
+    vigenereArray[i] = [];
+    for(int j = 0; j < cols; j++){
+      vigenereArray[i][j] = i + j;
+    }
+  }
+
+  for (int i = 0; i < word.length(); i++){
+    if (word[i] === " "){
+      decryptedWord += " ";
+      i++;
+    }
+    var letter = tempWord.charAt(i);
+    var wordNum = letter.charCodeAt(0) - 65;
+    var keyLetter = (key.toUpperCase()).charAt(keyCount);
+    var keyNum = keyLetter.charCodeAt(0) - 65;
+    var newNum = wordNum + keyNum;
+    if (newNum > 25){
+      newNum -= 26;
+    }
+    if ((word.charAt(i)).charCodeAt(0) > 64 && (word.charAt(i)).charCodeAt(0) < 91){
+      newNum += 65;
+    }
+    else{
+      newNum += 97;
+    }
+    encryptedWord += String.fromCharCode(newNum);
+  }
+  return encryptedWord;
+}
 export default App;
